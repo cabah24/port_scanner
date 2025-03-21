58981c47ffcc382ae6b36ac59902d27d697f0d5d
marti <marti.cabanas.gaza@gmail.com>
2025-03-21 04:22:02 +0100

    Especificaciones para casos de errores

--- port_scanner.py
+++ port_scanner.py
@@ -1,16 +1,22 @@
 import socket
 
 def scan_ports(target, ports):
-    print(f"\n Scanning {target}...")
+    print(f"\nScanning {target}...")
     for port in ports:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)  #Timeout of 1 second per port
+        sock.settimeout(1)  # Timeout of 1 second per port
         result = sock.connect_ex((target, port))
         if result == 0:
-            print(f" Port {port} igit s OPEN")
+            print(f"Port {port} is OPEN")
+        elif result == 111:
+            print(f"Port {port} is CLOSED (Connection Refused)")
+        elif result == 110:
+            print(f"Port {port} is CLOSED (Timeout)")
+        else:
+            print(f"Port {port} status unknown, result code: {result}")
         sock.close()
 
 if __name__ == "__main__":
     target_ip = input("Enter target IP: ")
-    ports_to_scan = range(1, 1025)  #Scan the first 1024 ports
+    ports_to_scan = [22, 80, 443]  # Ports to scan
     scan_ports(target_ip, ports_to_scan)
